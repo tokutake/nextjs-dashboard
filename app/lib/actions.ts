b1b8eb692b978acee1749b50a70f5854b86a43a6
Hokuto Tokutake <tokutake.hokuto@gmail.com>
2025-07-31 10:57:41 +0900

    Handle error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -26,10 +26,15 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
-  await sql`
-   INSERT INTO invoices (customer_id, amount, status, date)
-   VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
- `;
+  try {
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+  } catch (error) {
+    console.error('Error creating invoice:', error);
+    throw new Error('Failed to create invoice');
+  }
 
  revalidatePath('/dashboard/invoices');
  redirect('/dashboard/invoices');
@@ -46,12 +51,16 @@ export async function updateInvoice(id: string, formData: FormData) {
  
   const amountInCents = amount * 100;
  
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
- 
+  } catch (error) {
+    console.error('Error updating invoice:', error);
+  }
+
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
